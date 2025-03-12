dd6a2023bd38c84bf31b112b5318df3b603070c6
Kirill Vakalov <102545999+kirillvakalov@users.noreply.github.com>
2025-03-12 16:08:16 +0600

    Use libnut-linux_arm64 package on linux arm64

--- providers/libnut/import_libnut.ts
+++ providers/libnut/import_libnut.ts
@@ -3,7 +3,7 @@ import ln from "./libnut";
 const libnut: typeof ln = (process.platform === 'win32') ?
     require("@kirillvakalov/nut-tree__libnut-win32") :
     (process.platform === 'linux') ?
-        require("@kirillvakalov/nut-tree__libnut-linux") :
+        (process.arch === 'arm64') ? require("@kirillvakalov/nut-tree__libnut-linux_arm64") : require("@kirillvakalov/nut-tree__libnut-linux") :
         require("@kirillvakalov/nut-tree__libnut-darwin")
 
 export {
